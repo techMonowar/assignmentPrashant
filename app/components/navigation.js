facc52080a5d7a4107cacfcd8a8bb55e9deb468e
Monowar Hussain <monowargtp@gmail.com>
2024-05-22 19:31:53 +0530

    remove files

--- app/components/navigation.js
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-
-export default function Navigation() {
-  return (
-    <h1 className="bg-blue-900">HU</h1>
-  );
-}
--- app/components/promptbar.js
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-
-export default function Promptbar() {
-  return (
-    <h1 className="bg-blue-900">HU</h1>
-  );
-}
